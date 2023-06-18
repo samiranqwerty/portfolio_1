85d623537d10e26473d9ff7d2ad3c756ca48e35c
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2023-06-18 15:17:12 +0530

    image tag removed

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
